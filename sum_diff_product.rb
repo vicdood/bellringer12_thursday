b364cf10b731baf380caf54372ddf6d0f8e03a96
vicdood <vplascencia@chicagotechacademy.org>
2013-11-21 14:39:51 -0600

    lol

--- sum_diff_product.rb
+++ sum_diff_product.rb
@@ -1,28 +1,38 @@
-def apple(5,2)
-puts"Enter two numbers"
-5 get.chomp.to_i
-2 get.chomp.to_i
+class Numbers
+        def initialize(first_number, second_number)
+                @first_number  = first_number
+                @second_number = second_number
+        end
 
+        attr_accessor 'first_number', 'second_number'
 
-puts 5+2
-puts 5-2
-puts 5*2
+        def sum
+                @first_number + @second_number
+        end
+
+        def diff
+                @first_number - @second_number
+        end
+
+        def product
+                @first_number * @second_number
+        end
 
 end
 
 
+puts "enter first number"
+first_number = gets.chomp.to_i
 
+puts "enter second number"
+second_number = gets.chomp.to_i
 
+result = Numbers.new first_number, second_number
 
+puts ""
 
+puts result.sum
 
+puts result.diff
 
-
-
-
-
-
-
-
-
-
+puts result.product
